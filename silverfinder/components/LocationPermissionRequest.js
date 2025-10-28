cf725ac170103d9fce24150a2a1eb99a1431e3e8
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2025-10-27 23:20:31 -0500

    Create LocationPermissionRequest.js

---
+++ silverfinder/components/LocationPermissionRequest.js
@@ -0,0 +1,7 @@
+//Location permission request component 
+//Created on 10/27/25 by Rachel Townsend
+
+import React, {} from 'react';
+import {} from 'react-native';
+import * as Location from 'expo-location';
+
