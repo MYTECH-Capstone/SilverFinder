45514024a205802a1c518f84fa0eeb1db234717a
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2025-11-18 16:12:51 -0600

    Create useLocation.js

---
+++ silverfinder/src/hooks/useLocation.js
@@ -0,0 +1,29 @@
+//Created 11/18/2025 - Rachel Townsend
+//Don't forget to install expo-location "npx expo install expo-location"
+
+import {useState, useEffect, useRef} from 'react';
+import * as Location from 'expo-location';
+
+export default function useLocation(){
+
+}
+
+const getCurrentLocation = async () => {
+    try{
+        setIsLoading(true);
+        setError(null);
+    }
+
+    const currentLocation = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+    });
+
+    const locationData = {
+        latitude: 
+        longitude: 
+        accuracy: 
+        heading:
+        speed: 
+        timestamp:
+    };
+}
