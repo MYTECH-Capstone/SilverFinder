ca603a00d4fcf9939eda21856233937b9f775a0e
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2025-11-19 19:54:36 -0600

    Update useLocation.js

--- silverfinder/src/hooks/useLocation.js
+++ silverfinder/src/hooks/useLocation.js
@@ -3,6 +3,7 @@
 
 import {useState, useEffect, useRef} from 'react';
 import * as Location from 'expo-location';
+import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
 
 export default function useLocation(){
 
@@ -33,5 +34,55 @@ const getCurrentLocation = async () => {
     return locationData;
 } 
 catch(err){
-    
+    console.error('ERROR getting your current location:', err);
+    setError(err.message);
+    setIsLoading(false);
+    return null;
+}
+
+//Regular updates
+const startWatchingLocation = async () => {
+    try{
+        setError(null);
+
+        const {status} = await Location.getForegroundPermissionsAsync();
+        if(status !== 'granted'){
+            setError('Location permission not granted');
+            return;
+        }
+        if(LocationSubscription.current){
+            locationSubscription.current.remove();
+        }
+
+        locationSubscription.current = await Location.watchPositionAsync(
+            {
+                accuracy: Location.Accuracy.High,
+                timeInterval: 10000, //10 seconds?
+                distanceInterval: 10, //change later potentially
+            },
+            (newLocation) => {
+                const locationData = {
+                    latitude: 
+                    longitude:
+                    accuracy:
+                    altitude:
+                    heading:
+                    speed:
+                    timestamp:
+                };
+
+                setLocation(locationData);
+                setIsLoading(false);
+            }
+        );
+    }
+    catch(err){
+
+    }
+};
+
+const stopWatchingLocation
+
+return{
+    location, error, 
 }
