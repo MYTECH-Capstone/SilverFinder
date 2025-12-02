a5e45ae9f394c10e1a0e8a77994ff32ec71f8e2f
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2025-12-02 10:08:07 -0600

    Update Map.js

--- silverfinder/src/components/Map.js
+++ silverfinder/src/components/Map.js
@@ -36,6 +36,24 @@ export default function LocationMap(){
         }
     }, [location, followUser, mapReady]);
 
-    const handleRecenter = () =>
+    const handleRecenterPress = () => {
+        if(location && mapRef.current){
+            setFollowUser(true);
+            mapRef.current.animateToRegion({
+                latitude: location.latitude,
+                longitude: location.longitude,
+                latitudeDelta: 0.01,
+                longitudeDelta: 0.01,
+            }, 500);
+        } else{
+            Alert.alert('Location is not avaliable', 'Waiting for location data...');
+        }
+    };
+
+    const handleMapPress = () =>{}
+
+    const initialRegion = location
+
+    
 
 }
