01185a34023b030c81f2196548b6567d522e62f0
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2025-12-03 23:49:42 -0600

    comment out old location display

--- silverfinder/src/components/LocationDisplay.js
+++ silverfinder/src/components/LocationDisplay.js
@@ -1,5 +1,5 @@
 //Created on 11.19.25 - Rachel Townsend
-
+/*
 import React, {useEffect} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, ScrollView, ActivityIndicator} from 'react-native';
 import useLocation from '../hooks/useLocation';
@@ -283,3 +283,4 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
 });
+*/
