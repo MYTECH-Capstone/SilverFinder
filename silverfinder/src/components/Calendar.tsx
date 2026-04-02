1f4cc27f9099af04f99cb62a55f007fe3d95da60
Rachel Townsend <109994172+racheltown@users.noreply.github.com>
2026-04-01 20:30:33 -0500

    Update Calendar.tsx

--- silverfinder/src/components/Calendar.tsx
+++ silverfinder/src/components/Calendar.tsx
@@ -1,3 +1,4 @@
+//this is a comment by Rachel
 import React, { useState, useEffect } from "react";
 import { View, TouchableOpacity, Text, StyleSheet } from "react-native";
 import { Calendar, toDateId } from "@marceloterreiro/flash-calendar";
